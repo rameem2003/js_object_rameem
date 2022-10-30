dc0baabfc2bd7bc15b8c4c43cbcbea1bede68399
rameem2003 <rameem2019@gmail.com>
2022-10-30 19:56:53 +0600

    update

---
+++ my_api_file.js
@@ -0,0 +1,57 @@
+const me = {
+    mySelf : {
+        myName : "Mahmood Hassan Rameem",
+        age : 20,
+        birthday : "04 November 2003",
+        favouriteFood : "Biriani",
+        myFamily : {
+            father: {
+                name : "ANM Mudassir Hossain",
+                age : 60,
+            },
+
+            mother : {
+                name : "Nijhum Akter",
+                age : 40,
+            },
+
+            grandMother : {
+                name : "Mosreba Khathun",
+                age : 80, 
+            },
+
+            unclesAndanties : {
+                anty : {
+                    name : "Afroza Sultana",
+                    age : 50,
+                },
+            },
+
+            siblings : {
+                youngerBrother : {
+                    name : "Meherab Hossain Taneem",
+                    age : "14",
+                    hobby : ["Eat", "Outing"],
+                }
+            },
+
+
+        },
+
+        myFriends : {
+            friend1 : {
+                name : "Fahmida Yeasmin",
+                friendStatus : "Best Friend",
+                age : 22,
+                msg : "",
+            },
+
+            friend2 : {
+                name : "Mahabul Alam",
+                friendStatus : "My inocent good friend",
+                age : 20,
+                msg : "",
+            }
+        }
+    }
+}
