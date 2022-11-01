3721db8de0fa28c6b7941c19a3a7f4082a76cbd1
rameem2003 <rameem2019@gmail.com>
2022-11-01 22:07:45 +0600

    update

--- my_api_file.js
+++ my_api_file.js
@@ -4,6 +4,7 @@ const me = {
         age : 20,
         birthday : "04 November 2003",
         favouriteFood : "Biriani",
+        myImg : 'https://rameem2003.github.io/oursite/img/gallery/MAHMOOD%20HASSAN%20RAMEEM%201.jpg',
         myFamily : {
             father: {
                 name : "ANM Mudassir Hossain",
@@ -38,49 +39,51 @@ const me = {
 
         },
 
-        myFriends : {
-            friend1 : {
-                name : "Fahmida Yeasmin",
-                friendStatus : "Best Friend",
-                age : 22,
-                msg : "I will always with you Rameem. Love you alavola vai 🥰",
-                img : 'https://rameem2003.github.io/oursite/img/gallery/fahmida.jpg'
-            },
+    },
 
-            friend2 : {
-                name : "Mahabul Alam",
-                friendStatus : "My inocent good friend like brother",
-                age : 20,
-                msg : "প্রচুর মুখস্ত করতে হবে।",
-                img : 'https://rameem2003.github.io/oursite/img/gallery/mahabul_alam.jpg'
-            },
+    myFriends : {
+        friend1 : {
+            name : "Fahmida Yeasmin",
+            friendStatus : "Best Friend",
+            age : 22,
+            msg : "I will always with you Rameem. Love you alavola vai 🥰",
+            img : 'https://rameem2003.github.io/oursite/img/gallery/fahmida.jpg'
+        },
 
-            friend3 : {
-                name : "Jahedul Islam Rasel",
-                friendStatus : "My noakhailla brother",
-                age : 20,
-                msg : "খিদা লাগছে চল বাতেনকে চুনা দেই",
-                img : 'https://rameem2003.github.io/oursite/img/gallery/jahid_rasel.jpg'
-            },
+        friend2 : {
+            name : "Mahabul Alam",
+            friendStatus : "My inocent good friend like brother",
+            age : 20,
+            msg : "প্রচুর মুখস্ত করতে হবে।",
+            img : 'https://rameem2003.github.io/oursite/img/gallery/mahabul_alam.jpg'
+        },
 
-            friend4 : {
-                name : "Pabel Pathan",
-                friendStatus : "Stylish Man",
-                age : 20,
-                msg : "cholo jai k te kacchi te",
-                img : 'https://rameem2003.github.io/oursite/img/gallery/pathan.jpg'
-            },
+        friend3 : {
+            name : "Jahedul Islam Rasel",
+            friendStatus : "My noakhailla brother",
+            age : 20,
+            msg : "খিদা লাগছে চল বাতেনকে চুনা দেই",
+            img : 'https://rameem2003.github.io/oursite/img/gallery/jahid_rasel.jpg'
+        },
+
+        friend4 : {
+            name : "Pabel Pathan",
+            friendStatus : "Stylish Man",
+            age : 20,
+            msg : "cholo jai k te kacchi te",
+            img : 'https://rameem2003.github.io/oursite/img/gallery/pathan.jpg'
+        },
 
 
-            friend5 : {
-                name : "Abdul Baten",
-                friendStatus : "Our ATM Card bro",
-                age : 20,
-                msg : "টাকা নাই 😒 ( সবচেয়ে বড় গুজব )",
-                img : 'https://rameem2003.github.io/oursite/img/gallery/baten.jpg'
-            },
-        }
+        friend5 : {
+            name : "Abdul Baten",
+            friendStatus : "Our ATM Card bro",
+            age : 20,
+            msg : "টাকা নাই 😒 ( সবচেয়ে বড় গুজব )",
+            img : 'https://rameem2003.github.io/oursite/img/gallery/baten.jpg'
+        },
     }
 }
 
-console.log(me.mySelf.myFriends);
+
+console.log(me.myFriends);
